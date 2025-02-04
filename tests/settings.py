2c317b02a289d40623624ec290ef61f9b78e36aa
Abdulwasiu Apalowo <64538336+mrbazzan@users.noreply.github.com>
2025-02-04 17:28:29 +0100

    chore: Remove `django-app-helper` (#76)
    * chore: Remove django-app-helper
    
    * ruff: Remove setup.py specific config
    
    * ruff: Ignore naming convention in runtests.py
    
    * chore: Update test requirements

--- tests/settings.py
+++ tests/settings.py
@@ -1,21 +1,73 @@
-#!/usr/bin/env python
-HELPER_SETTINGS = {
-    "INSTALLED_APPS": [],
-    "CMS_LANGUAGES": {
-        1: [{
-            "code": "en",
-            "name": "English",
-        }]
+import os
+
+SECRET_KEY = "djangocms-style-test"
+
+ALLOWED_HOSTS = ["localhost"]
+
+INSTALLED_APPS = [
+    "django.contrib.contenttypes",
+    "django.contrib.auth",
+    "django.contrib.sites",
+    "django.contrib.sessions",
+    "django.contrib.admin",
+    "django.contrib.messages",
+
+    "cms",
+    "menus",
+    "treebeard",
+    "djangocms_style",
+]
+
+MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            os.path.join((os.path.dirname(__file__)), "templates"),
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
     },
-    "LANGUAGE_CODE": "en",
-    "ALLOWED_HOSTS": ["localhost"],
+]
+
+SITE_ID=1
+
+CMS_TEMPLATES = (
+    ("page.html", "Normal page"),
+)
+
+CMS_LANGUAGES = {
+    1: [{
+        "code": "en",
+        "name": "English",
+    }]
 }
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "mydatabase",
+        "TEST": {
+            # disable migrations when creating test database
+            "MIGRATE": False,
+        },
+    }
+}
 
-def run():
-    from app_helper import runner
-    runner.cms("djangocms_style")
+LANGUAGE_CODE = "en"
 
+ROOT_URLCONF = "tests.urls"
 
-if __name__ == "__main__":
-    run()
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
